e74021c509d6f9c7911abf5cfe868d7540e5cbf2
unknown <sumeerajeelankhan@gmail.com>
2024-07-15 22:56:19 +0530

    changed by js

---
+++ tablehj.js
@@ -0,0 +1,13 @@
+document.write("<table><thead><th>FIRST NAME<\th><th>LAST NAME<\th><th>GENDER<\th><th>LINKED PROFILE<\th></thead>");
+for(var i=0;i<1;i++)
+{
+    for(var j=0;j<1;j++)
+    {
+        document.write("<tr><td>sumeera</td><td>jeelankhan</td><td>female</td></tr>");
+        document.write("<tr><td>shakshi</td><td>priya</td><td>female</td></tr>");
+        document.write("<tr><td>helan</td><td>jenifer</td><td>female</td></tr>");
+        document.write("<tr><td>sowmiya</td><td>sankaran</td><td>female</td></tr>");
+    };
+}
+document.write("</table>");
+
